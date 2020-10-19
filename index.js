b1afc62f582120c4a3005bf3ec640e606332957d
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2020-10-19 16:53:52 -0600

    broke code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('missing Semicolin.');
-console.log('wrong quotes');
-console.log('wrong indenting');
-//I modifed this code here to trigger a check
+console.log('missing Semicolin.')
+console.log("wrong quotes");
+        console.log('wrong indenting');
