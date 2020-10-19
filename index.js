daed208fc1f718b6091d4a6f858387f3d32a7cbb
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2020-10-19 23:35:24 +0000

    [ESLint] Auto formatting.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('missing Semicolin.')
-console.log("wrong quotes");
-        console.log('wrong indenting');
+console.log('missing Semicolin.');
+console.log('wrong quotes');
+console.log('wrong indenting');
 // reload
