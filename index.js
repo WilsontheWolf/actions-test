199773ecfaa0151c367afc39fbfa218f0f8baf61
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2020-10-19 16:09:47 -0600

    bad code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('missing Semicolin.');
-console.log('wrong quotes');
-console.log('wrong indenting');
-//I modifed this code here to trigger a check
+console.log('missing Semicolin.')
+console.log("wrong quotes");
+    console.log('wrong indenting');
+    //I modifed this code here to trigger a check
